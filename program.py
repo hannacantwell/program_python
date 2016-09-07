83273386125ccca920564925dd4455092dc0eda3
hannacantwell <hanna.cantwell@bbm.bonnier.se>
2016-09-07 13:38:26 +0200

    Init nummer 2 - works!

--- program.py
+++ program.py
@@ -1,12 +1,14 @@
 class Product:
-	def__init__(self, price, count,tax):
+	def __init__(self, price, count, tax):
 		self.price = price
-		self.count= count
+		self.count = count
 		self.tax = tax
 
-robot = Prouct(price=900, count=2, tax=1.25)
-book = Prodcut(price=100,count=1, tax=1.06)
+	def price_with_tax(self):
+		return self.price * self.count * self.tax
 
+robot = Product(price = 900, count = 2, tax = 1.25)
+book = Product(price = 100, count = 1, tax = 1.06)
 
 
 print(robot.price_with_tax() + book.price_with_tax())
