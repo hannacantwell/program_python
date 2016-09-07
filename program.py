908d34a00fff0ea09028a2d32ee0ccbb98507cc8
hannacantwell <hanna.cantwell@bbm.bonnier.se>
2016-09-07 14:33:29 +0200

    loop_2

--- program.py
+++ program.py
@@ -10,15 +10,9 @@ class Product:
 
 products = [Product(price=900, count=2, tax=1.25), Product(price=100, count=1, tax=1.06)]
 
-total_price = 0
-
-for p in products:
-	total_price = total_price + p.price_with_tax()
-
-print(total_price)
-
 
 def price_with_tax(self)
 	total = self.price * self.count * self.tax
 	if total > 500:
 		return 0.9 * total
+
