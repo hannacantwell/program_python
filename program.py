36f1113cfcf2f34cf96567ed761686cc38a40700
hannacantwell <hanna.cantwell@bbm.bonnier.se>
2016-09-07 14:39:03 +0200

    rabatt2

--- program.py
+++ program.py
@@ -10,9 +10,17 @@ class Product:
 
 products = [Product(price=900, count=2, tax=1.25), Product(price=100, count=1, tax=1.06)]
 
+total_price = 0
 
-def price_with_tax(self)
-	total = self.price * self.count * self.tax
-	if total > 500:
-		return 0.9 * total
+for p in products:
+	total_price = total_price + p.price_with_tax()
 
+
+def price_with_tax(self):
+	total_price = self.price * self.count * self.tax
+	if total_price > 500:
+		return 0.9 * total_price
+	else:
+		return total_price
+
+print(total_price)
