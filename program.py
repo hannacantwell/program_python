813eeeccc3880067d6dd28ecf99155d03eb917fb
hannacantwell <hanna.cantwell@bbm.bonnier.se>
2016-09-07 13:19:41 +0200

    Klass & metod

--- program.py
+++ program.py
@@ -1,11 +1,16 @@
-print(900 * 2 * 1.25 + 100 * 1.06)
+class Product:
+	def price_with_tax(self):
+		return self.price * self.count * self.tax	
 
-robot_price = 900
-robot_count = 2
-robot_tax = 1.25
+robot = Product()
+robot.price = 900
+robot.count = 2
+robot.tax = 1.25
 
-book_price = 100
-book_count = 1
-book_tax = 1.06
 
-print (robot_price * robot_count * robot_tax + book_price * book_count * book_tax)
+book = Product()
+book.price = 100
+book.count = 1
+book.tax = 1.06 
+
+print(robot.price_with_tax() + book.price_with_tax())
